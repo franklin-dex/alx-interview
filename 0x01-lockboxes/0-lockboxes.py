52ddc73b582bc1026da2a7d0f2d7e16ba54aa8a8
Akamere Franklin <140992757+franklin-dex@users.noreply.github.com>
2024-08-02 02:00:30 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle using Union-Find"""
-
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [1] * size
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
+"""Solves the lock boxes puzzle using an iterative DFS approach"""
 
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes can be opened using Union-Find.
+    Determines if all the boxes can be opened.
 
     Parameters:
     boxes: A list where each element is a list of keys contained in that box.
@@ -34,20 +12,18 @@ def canUnlockAll(boxes):
     bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
-    uf = UnionFind(n)
-    visited = [False] * n
-    visited[0] = True
+    opened = [False] * n
+    opened[0] = True
     stack = [0]
 
     while stack:
         box_index = stack.pop()
         for key in boxes[box_index]:
-            if key < n and not visited[key]:
-                visited[key] = True
+            if key < n and not opened[key]:
+                opened[key] = True
                 stack.append(key)
-                uf.union(box_index, key)
 
-    return len(set(uf.find(i) for i in range(n))) == 1
+    return all(opened)
 
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
