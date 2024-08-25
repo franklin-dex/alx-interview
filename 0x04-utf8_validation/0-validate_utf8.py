9ced6b8b0352e89bacb046af4acb3a8827142f0d
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-08-25 02:17:25 +0100

    add method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,14 +7,14 @@ method that determines if a given data set represents a valid UTF-8 encoding
 def validUTF8(data):
     """
     Determine if a given data set represents a valid UTF-8 encoding.
-    
+
     Args:
     data: List of integers, each representing a byte.
-    
+
     Returns:
     True if data is a valid UTF-8 encoding, otherwise False.
     """
-    
+
     '''Number of bytes in the current UTF-8 character'''
     num_bytes = 0
 
@@ -30,21 +30,21 @@ def validUTF8(data):
             while mask & byte:
                 num_bytes += 1
                 mask >>= 1
-            
+ 
             '''1-byte characters or invalid UTF-8'''
             if num_bytes == 0:
                 continue
 
-            '''Invalid UTF-8, the first byte must start with 2, 3, or 4 bytes long'''
+            '''Invalid UTF-8, first byte must start with 2, 3, or 4 bytes long'''
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
             '''Check that the current byte is a continuation byte: 10xxxxxx'''
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-        
+
         '''Reduce the number of bytes remaining to check'''
         num_bytes -= 1
-    
+
     '''If all characters are valid UTF-8, num_bytes should be zero'''
     return num_bytes == 0
